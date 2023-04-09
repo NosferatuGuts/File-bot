8c79c486d19ee8f0a2936d53950d31e310b713ba
Ryuuzai <116559457+dragozai@users.noreply.github.com>
2023-04-09 06:00:52 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,7 +83,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", url = "https://t.me/Kazukai"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
